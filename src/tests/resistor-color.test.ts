31a3da8fe38063063b88196522958606505c2859
Iamouma <stephenouma452@gmail.com>
2025-02-07 15:18:50 +0300

    The goal is to look up the numerical value associated with a particular color band

---
+++ src/tests/resistor-color.test.ts
@@ -0,0 +1,33 @@
+import { describe, xdescribe, it, expect, xit } from '@jest/globals'
+import { colorCode, COLORS } from '../exercises/resistor-color'
+
+describe('color code', () => {
+  it('Black', () =>{
+    expect(colorCode('black')).toEqual(0)
+  })
+
+  xit('White', () => {
+    expect(colorCode('white')).toEqual(9)
+  })
+
+  xit('Ornage', () => {
+    expect(colorCode('orange')).toEqual(3)
+  })
+})
+
+xdescribe('Colors', () => {
+  xit('returns all colors', () => {
+    expect(COLORS).toEqual([
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ])
+  })
+})
