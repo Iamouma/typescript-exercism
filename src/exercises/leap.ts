0a3a2fcfdcf0f9762fdf9bf785d591de144ef221
Stephen Ouma <141272811+Iamouma@users.noreply.github.com>
2025-02-07 17:42:18 +0300

    Update leap.ts

--- src/exercises/leap.ts
+++ src/exercises/leap.ts
@@ -9,18 +9,14 @@
  * @returns {boolean} - Returns true if it's a leap year, otherwise false.
  */
 export function isLeap(year: number): boolean {
-  // If the year is divisible by 400, it's a leap year
   if (year % 400 === 0) {
     return true;
   }
-  // If the year is divisible by 100 but NOT by 400, it's NOT a leap year
   if (year % 100 === 0) {
     return false;
   }
-  // If the year is divisible by 4, it's a leap year
   if (year % 4 === 0) {
     return true;
   }
-  // Otherwise, it's NOT a leap year
   return false;
 }
