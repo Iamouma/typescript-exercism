23ddb10694e00ba098fe0965fe469b9d2d5dd57f
Iamouma <stephenouma452@gmail.com>
2025-02-10 12:04:48 +0300

    A clock that handles times without dates

---
+++ src/exercises/clock.ts
@@ -0,0 +1,29 @@
+export class Clock {
+  private minutes: number;
+
+  constructor(hour: number, minute: number = 0) {
+    this.minutes = (hour * 60 + minute) % 1440;
+    if (this.minutes < 0) {
+      this.minutes += 1440;
+    }
+  }
+
+  public toString(): string {
+    const hours = Math.floor(this.minutes / 60);
+    const mins = this.minutes % 60;
+    return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}`;
+  }
+
+  public plus(minutes: number): Clock {
+    return new Clock(0, this.minutes + minutes);
+  }
+
+  public minus(minutes: number): Clock {
+    return new Clock(0, this.minutes - minutes);
+  }
+
+  public equals(other: Clock): boolean {
+    return this.minutes === other.minutes;
+  }
+}
+
